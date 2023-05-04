6eee46d012c7c6dada8e7b8ad9c73e8a1da6d951
Yash <imyashpawar@gmail.com>
2023-05-04 23:29:31 +0530

    Added: uc2 Last Name Validation

--- src/com/bridgelabz/day19/RegexAssignement.java
+++ src/com/bridgelabz/day19/RegexAssignement.java
@@ -5,11 +5,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexAssignement {
-
-    public static void main(String[] args) {
-
-        System.out.println("Regex Assignment");
-
+    
+    public void validateFirstName(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your First Name: ");
         String firstName= sc.next();
@@ -21,10 +18,20 @@ public class RegexAssignement {
         }else {
             System.out.println("Invalid First Name");
         }
-
-
-
-
-
     }
+
+
+    public void validateLastName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your Last Name: ");
+        String lastName = sc.next();
+        Pattern pattern = Pattern.compile("^[A-Z]{3}$");
+        Matcher matcher = pattern.matcher(lastName);
+        if (matcher.matches()){
+            System.out.println("Valid Last Name");
+        }else {
+            System.out.println("Invalid Last Name");
+        }
+    }
+
 }
---
+++ src/com/bridgelabz/day19/RegexAssignementMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.day19;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexAssignementMain {
+
+    public static void main(String[] args) {
+
+       RegexAssignement validation = new RegexAssignement();
+
+       validation.validateFirstName();
+       validation.validateLastName();
+
+
+    }
+}
